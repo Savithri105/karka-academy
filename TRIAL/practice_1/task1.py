fdecc1d000908b5763049e6a1e6cb545eac5713b
Savithri105 <ramswethaa915@gmail.com>
2025-05-14 11:06:30 +0100

    task1

---
+++ TRIAL/practice_1/task1.py
@@ -0,0 +1,80 @@
+#1
+print("Hello,world!")
+#2
+print("Savithri")
+#3
+Name="Savithri"
+Age=22
+print("Name:", Name)
+print("Age:",Age)
+#4
+print("Name:Savithri")
+print("Age:",22)
+print("Grade=",80.6)
+a=True
+print(a)
+#5
+a="Savithri"
+b=22
+print("My name is " + a + " and my age is " + b)
+#6
+Name="Savithri"
+Age=22
+print("Name",Name)
+print("age",Age)
+#7
+a=10
+b=20
+c=a+b
+print(c)
+
+a=10
+b=30
+c=a*b
+print(c)
+
+a=10
+b=25
+c=a-b
+print(c)
+
+a=10
+b=20
+c=a/b
+print(c)
+#8
+a=["*" , "**", "***", "****"]
+for i in a:
+    print(i)
+
+print("*")
+print("**")
+print("***")
+print("****")
+#9
+name="Savithri"
+age=22
+isStudent=True
+print("Name",name)
+print("Age",age)
+print("is_student",isStudent)
+name="Swetha"
+age_nxtyear=age+1
+isStudent=not isStudent
+print("Updated name",name)
+print("Updated age",age_nxtyear)
+print("Updated isStudent",isStudent)
+#10
+Name="Savithri"
+age=22
+Dob="26-10-2002"
+height=157.5
+weight=45.3
+degree="Bsc Agriculture"
+gender="Female"
+print("My name is:",Name)
+print("my age is:",age)
+print("my dob is:",Dob)
+prin/t("my height is:",height)
+print("my weight is:",weight)
+print("my gender is",gender)
